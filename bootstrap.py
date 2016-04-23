b63d670072795156acc5aaa3233ba778b50159ef
Charles Bushong <cbushong@fearsol.com>
2016-04-23 14:34:54 -0400

    Framework for aws client

--- bootstrap.py
+++ bootstrap.py
@@ -3,13 +3,18 @@ import settings
 
 from scripts import fearless_github
 from scripts import slack
+from scripts import ec2
 
 project_name = "devops-autocreate-3"
 
 print "Calling github script..."
-fearless_github.create_repo(project_name)
+#fearless_github.create_repo(project_name)
 print "Done github!"
 
 print "Creating slack channel..."
-slack.create_channel(project_name)
+#slack.create_channel(project_name)
 print "Done slack!"
+
+print "Creating EC2..."
+#
+print "Done ec2!"
--- scripts/aws.py
+++ scripts/aws.py
@@ -1,3 +1,47 @@
-#!/usr/bin/env python
+import os
+import boto3
+import re
 
 # Will launch an EC2
+def launch_ec2(name){
+    stack_name = "DevOps-AutoCreate-"+re.sub("[^0-9a-zA-Z]","-",name)
+    client = boto3.client(
+	'cloudformation',
+	aws_access_key_id=os.environ.get('AWS_KEY'),
+	aws_secret_access_key=environ.get('AWS_SECRET')
+    }
+    response = client.create_stack(
+	StackName='DevOps-Autocreate-'+name,
+	TemplateBody='string',
+	TemplateURL='string',
+	Parameters=[
+	    {
+		'ParameterKey': 'string',
+		'ParameterValue': 'string',
+		'UsePreviousValue': True|False
+	    },
+	],
+	DisableRollback=True|False,
+	TimeoutInMinutes=123,
+	NotificationARNs=[
+	    'string',
+	],
+	Capabilities=[
+	    'CAPABILITY_IAM',
+	],
+	ResourceTypes=[
+	    'string',
+	],
+	OnFailure='DO_NOTHING'|'ROLLBACK'|'DELETE',
+	StackPolicyBody='string',
+	StackPolicyURL='string',
+	Tags=[
+	    {
+		'Key': 'string',
+		'Value': 'string'
+	    },
+	]
+    )
+}
+
+
